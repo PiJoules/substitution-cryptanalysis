e987735c9f0e831c97fade05728e7a0c645724c0
Leo is awesome <lchan1994@yahoo.com>
2015-07-06 20:00:34 -0400

    gotta increase that github score

--- substitution-cryptanalysis.c
+++ substitution-cryptanalysis.c
@@ -37,23 +37,6 @@ void printCipher(char* cipher){
 }
 
 /**
- * Find all available ciphers.
- */
-int unscramble(char* ciphers, char* words, int wordCount, int depth, char* cipher, char* dictionary, int dictionarySize, int maxWordLength){
-	if (depth >= wordCount){
-		ciphers = (char*)realloc( ciphers, 26*sizeof(char) );
-		memcpy(ciphers, cipher, 26);
-		return 1;
-	}
-
-	char* possibleWords = (char*)malloc(1);
-
-	free(possibleWords);
-
-	return 1;
-}
-
-/**
  * Just check the case.
  */
 int isUpper(char c){
@@ -73,6 +56,7 @@ int wordMatchesFormat(char* word, char* format){
 	if (wordLen != formatLen)
 		return 0;
 
+	char formatMap[1024] = {0};
 	for (i = 0; i < formatLen; i++){
 		char c = *(format + i);
 		if (isLower(c)){ // found a known letter
@@ -81,21 +65,108 @@ int wordMatchesFormat(char* word, char* format){
 				return 0;
 			}
 		}
+		else if (isUpper(c)){ // make sure the format matches
+			char index = c-'A';
+			if (!formatMap[index]){
+				formatMap[index] = *(word + i); // match the uppercase letter to the current lowercase letter
+			}
+			else if (formatMap[index] != *(word + i)){
+				return 0; // if the current lowercase does not match a previously matched one, no match
+			}
+		}
 	}
 	return 1;
 }
 
-int getPossibleWords(char* possibleWords, char* word, char* cipher, char* dictionary, int dictionarySize, int maxWordLength){
+/**
+ * Save all possible words that match te given format.
+ */
+int getPossibleWords(char** possibleWords, char* format, char* dictionary, int dictionarySize, int maxWordLength){
 	int i;
 	int count = 0;
 	for (i = 0; i < dictionarySize; i++){
 		if (wordMatchesFormat( dictionary+i*maxWordLength, format )){
-			possibleWords = realloc( possibleWords, ( (count+1)*maxWordLength )*sizeof(char) );
-			// start herestrncpy(pos + count*maxWordLength, dictionary+i*maxWordLength, maxWordLength);
+			*possibleWords = (char*)realloc( *possibleWords, ( (count+1)*(maxWordLength) )*sizeof(char) );
+			memcpy(*possibleWords + count*maxWordLength, dictionary + i*maxWordLength, maxWordLength);
 			count++;
 		}
 	}
 	return count;
+}
+
+/**
+ * Before is the format (upper or lowercase). After is the possible word from the dict (all lowercase).
+ */
+int cipherFromMap(char* before, char* after, char* base, char* newCipher){
+	int i, j;
+	int len = strlen(before);
+	for (i = 0; i < 26; i++){
+		*(newCipher + i) = *(base + i);
+	}
+	for (i = 0; i < len; i++){
+		char b = *(before + i);
+		char a = *(after + i);
+		if (isUpper(b)){
+			// If the value exists in the cipher and it is not equal to the corresponding letter
+			int letterIndex = b - 'A';
+			if ( *(newCipher + letterIndex) ){
+				if ( *(newCipher + letterIndex) != a ){
+					return 0;
+				}	
+			}
+			*(newCipher + letterIndex) = a;
+		}
+	}
+
+	// Check for any duplicates
+	for (i = 0; i < 26; i++){
+		for (j = i+1; j < 26; j++){
+			if ( *(newCipher+i) ){
+				if ( *(newCipher+i) == *(newCipher+j) ) // found dup
+					return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+/**
+ * Find all available ciphers.
+ */
+int unscramble(char** ciphers, char* words, int wordCount, int depth, char* cipher, char* dictionary, int dictionarySize, int maxWordLength){
+	if (depth >= wordCount){
+		*ciphers = (char*)realloc( *ciphers, 26*sizeof(char) );
+		memcpy(*ciphers, cipher, 26);
+		return 1;
+	}
+
+	int i;
+	char* possibleWords = (char*)malloc(1);
+	int cipherCount = 0;
+	int matchCount = getPossibleWords(&possibleWords, words + depth*maxWordLength, dictionary, dictionarySize, maxWordLength);
+	for (i = 0; i < matchCount; i++){
+		// option = possibleWords + i*maxWordLength
+		char newCipher[26] = {0};
+		int success = cipherFromMap(words + depth*maxWordLength, possibleWords + i*maxWordLength, cipher, newCipher);
+		if (success){
+			char* solutions = (char*)malloc(1);
+			int solutionCount = unscramble(&solutions, words, wordCount, depth+1, newCipher, (char*)dictionary, dictionarySize, maxWordLength);
+			
+			int j;
+			printf("found %d solutions\n", solutionCount);
+			for (j = 0; j < solutionCount; j++){
+				*ciphers = (char*)realloc( *ciphers, sizeof(char)*(cipherCount+1)*26 );
+				memcpy(*ciphers + j*26, solutions + j*26, 26);
+				cipherCount++;
+			}
+
+			free(solutions);
+		}
+	}
+	free(possibleWords);
+
+	return 0;
 }
 
 int main(){
@@ -135,17 +206,12 @@ int main(){
 
 	/* Do the thing */
 	char* solutions = (char*)malloc(1);
-	int solutionCount = unscramble(solutions, words, wordCount, 0, cipher, (char*)dictionary, DICTIONARYSIZE, MAXWORDLENGTH);
+	int solutionCount = unscramble(&solutions, words, wordCount, 0, cipher, (char*)dictionary, DICTIONARYSIZE, MAXWORDLENGTH);
+	printf("%d solutions\n", solutionCount);
 	for (i = 0; i < solutionCount; i++){
 		printCipher(solutions + i*26);
 	}
 	free(solutions);
-
-	/* Print stuff */
-	printCipher(cipher);
-	for (i = 0; i < wordCount; i++){
-		printf("word: %s\n", words + i*MAXWORDLENGTH);
-	}
 
 	return 0;
 }
